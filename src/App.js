fa8991d7f4ff41a44cedf90d4e206215316a466e
Joseph Madras <josephkabemba77@gmail.com>
2021-12-05 23:05:06 +0100

    feat: add leftweight and rightweight to SplitScreen component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import { SplitScreen } from "./SplitScreen";
 
-const LeftHandComponent = () => <h1>Left!</h1>;
-const RightHandComponent = () => <h1>Right!</h1>;
+const LeftHandComponent = () => (
+  <h1 style={{ backgroundColor: "green" }}>Left!</h1>
+);
+const RightHandComponent = () => (
+  <h1 style={{ backgroundColor: "red" }}>Right!</h1>
+);
 
 function App() {
-  return <SplitScreen left={LeftHandComponent} right={RightHandComponent} />;
+  return (
+    <SplitScreen
+      left={LeftHandComponent}
+      right={RightHandComponent}
+      leftWeight={1}
+      rightWeight={3}
+    />
+  );
 }
 
 export default App;
